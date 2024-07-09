017c4ffbd3140e3e5d8b4195e4b33d5b0704b100
fatmanurshain <shnfatmanurr@gmail.com>
2024-07-09 19:00:20 +0300

    Initial Commit

---
+++ LaunchScreenAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaunchScreenAnimation
+//
+//  Created by Fatmanur Şahin on 9.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
